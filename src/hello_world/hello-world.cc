a1c5d6e9f80f0bfb02688503cb06f888dbc374ed
Justin Williams <jwilliams@spinlaunch.com>
2024-03-19 00:16:13 +0000

    Semi-complete elf (i think) Switched to bzlmod Removed pigweed for now

--- src/hello_world/hello-world.cc
+++ src/hello_world/hello-world.cc
@@ -1,7 +1,15 @@
 
-#include <iostream>
+#include <stdio.h>
+
+#include "pico/stdlib.h"
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  stdio_init_all();
+
+  while (true) {
+    printf("Hello, world!\n");
+    sleep_ms(1000);
+  }
+
   return 0;
 }
