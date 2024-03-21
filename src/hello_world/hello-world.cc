ac71e0e6ddbb4465e22322e63b088ee18507dede
Justin Williams <jwilliams@spinlaunch.com>
2024-03-21 15:08:32 -0700

    Added flash script / hello-world blink Debugging without flashing the LED was pointless. Flash script works with Picoprobe.

--- src/hello_world/hello-world.cc
+++ src/hello_world/hello-world.cc
@@ -5,8 +5,13 @@
 
 int main() {
   stdio_init_all();
-
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+  
+  uint8_t status = 0;
   while (true) {
+    gpio_put(PICO_DEFAULT_LED_PIN, status);
+    status = !status;
     printf("Hello, world!\n");
     sleep_ms(1000);
   }
